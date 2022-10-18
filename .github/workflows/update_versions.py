af17e12e7175f3e159ed412c0ea5f076701e1645
ritwik-deshpande <ritwikdeshpande01@gmail.com>
2022-10-17 19:10:29 -0500

    Helm release - R

--- .github/workflows/update_versions.py
+++ .github/workflows/update_versions.py
@@ -1,12 +1,12 @@
-import yaml
 import sys
+import ruamel.yaml
 
 def update_version_in_servicex_chart_yaml(version):
     servicex_chart_yaml_file = "servicex/Chart.yaml"
     with open(servicex_chart_yaml_file, 'r') as stream:
         try:
-            servicex_chart_data = yaml.load(stream, Loader=yaml.SafeLoader)
-        except yaml.YAMLError as exc:
+            servicex_chart_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+        except ruamel.yaml.YAMLError as exc:
             print("ERR: loading Yaml file")
             raise Exception(exc)
 
@@ -14,14 +14,14 @@ def update_version_in_servicex_chart_yaml(version):
     servicex_chart_data['version'] = version
 
     with open(servicex_chart_yaml_file, 'w') as yaml_file:
-        yaml_file.write(yaml.dump(servicex_chart_data, default_flow_style=False))
+        yaml_file.write(ruamel.yaml.dump(servicex_chart_data, Dumper=ruamel.yaml.RoundTripDumper))
 
 def update_version_in_servicex_values_yaml(version):
     servicex_values_yaml_file = "servicex/values.yaml"
     with open(servicex_values_yaml_file, 'r') as stream:
         try:
-            servicex_values_data = yaml.load(stream, Loader=yaml.SafeLoader)
-        except yaml.YAMLError as exc:
+            servicex_values_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+        except ruamel.yaml.YAMLError as exc:
             print("ERR: loading Yaml file")
             raise Exception(exc)
 
@@ -33,8 +33,7 @@ def update_version_in_servicex_values_yaml(version):
     servicex_values_data['codeGen']['tag'] = version
 
     with open(servicex_values_yaml_file, 'w') as yaml_file:
-        yaml_file.write(yaml.dump(servicex_values_data, default_flow_style=False))
-
+        yaml_file.write(ruamel.yaml.dump(servicex_values_data,  Dumper=ruamel.yaml.RoundTripDumper))
 
 
 
@@ -42,13 +41,13 @@ def update_version_in_flux_river_uproot_values_yaml(version):
     flux_river_uproot_values_file = "../flux_river_configs/servicex-int-uproot/values.yaml"
     with open(flux_river_uproot_values_file, 'r') as stream:
         try:
-            flux_river_uproot_data = yaml.load(stream, Loader=yaml.SafeLoader)
-        except yaml.YAMLError as exc:
+            flux_river_uproot_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+        except ruamel.yaml.YAMLError as exc:
             print(exc)
 
     flux_river_uproot_data['spec']['chart']['spec']['version'] = version
     with open(flux_river_uproot_values_file, 'w') as yaml_file:
-        yaml_file.write(yaml.dump(flux_river_uproot_data, default_flow_style=False))
+        yaml_file.write(ruamel.yaml.dump(flux_river_uproot_data, Dumper=ruamel.yaml.RoundTripDumper))
 
 
 def update_version_in_flux_river_xaod_values_yaml(version):
@@ -56,14 +55,14 @@ def update_version_in_flux_river_xaod_values_yaml(version):
 
     with open(flux_river_xaod_values_file, 'r') as stream:
         try:
-            flux_river_xaod_data = yaml.load(stream, Loader=yaml.SafeLoader)
-        except yaml.YAMLError as exc:
+            flux_river_xaod_data = ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader)
+        except ruamel.yaml.YAMLError as exc:
             print(exc)
 
     flux_river_xaod_data['spec']['chart']['spec']['version'] = version
 
     with open(flux_river_xaod_values_file, 'w') as yaml_file:
-        yaml_file.write(yaml.dump(flux_river_xaod_data, default_flow_style=False))
+        yaml_file.write(ruamel.yaml.dump(flux_river_xaod_data, Dumper=ruamel.yaml.RoundTripDumper))
 
 
 if __name__ == '__main__':
